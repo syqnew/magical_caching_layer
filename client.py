98341f81b7ebd925daf1133543a771599f6c01c0
Kelly <kxzhang@mit.edu>
2015-11-09 19:15:01 -0500

    client side code

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+class Client():
+   def __init__(self,Adress=("18.189.106.35", 5000)):
+      self.s = socket.socket()
+      self.s.connect(Adress)
+
+   def send_message(self, msg):
+      self.s.send(msg.encode())
+
+def main():
+   TC=Client()
+
+   while True:
+      value = input("Enter key value pair:")
+
+      try:
+         message = str(value)
+         print("sending: message")
+         TC.send_message(message)
+      except ValueError:
+         print("Invalid Input")
+
+if __name__ == "__main__":
+   main()
