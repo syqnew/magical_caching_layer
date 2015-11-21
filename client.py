f07c45060511f99334d366b5752811d9226e71a9
Stephanie New <syqnew@gmail.com>
2015-11-21 11:48:42 -0500

    Added the design doc spec methods

--- client.py
+++ client.py
@@ -5,9 +5,14 @@ class Client():
    def __init__(self,Adress=("18.189.106.35", 5000)):
       self.s = socket.socket()
       self.s.connect(Adress)
-
+'''
    def send_message(self, msg):
       self.s.send(msg.encode())
+'''
+
+   def get(key):
+      self.s.send(key.encode())
+      # prints out to standard output
 
 def main():
    TC=Client()
